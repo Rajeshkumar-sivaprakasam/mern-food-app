79e3317b9e532ec5cd6a9e729bcc17d78870a6c9
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2024-02-17 12:50:59 +0530

    oauth added!

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './global.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { AppRoutes } from './appRoutes'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppRoutes } from "./appRoutes";
+import Auth0ProvoiderWithNavigation from "./auth/Auth0ProvoiderWithNavigation";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProvoiderWithNavigation>
+        <AppRoutes />
+      </Auth0ProvoiderWithNavigation>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

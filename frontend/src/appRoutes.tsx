4799e00a483339804d2a441f29669f64a5003e51
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2024-02-17 11:18:56 +0530

    basic header

--- frontend/src/appRoutes.tsx
+++ frontend/src/appRoutes.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+import { Layout } from "./layout"
 
 export const AppRoutes = () =>{
     return(
         <Routes>
-            <Route path="/" element={<span>Home page</span>} />
+            <Route path="/" element={<Layout><span>Home page</span></Layout>} />
             <Route path="/user-profile" element={<span>User Profile page</span>} />
             <Route path="*" element={<Navigate to={'/'} />} />
         </Routes>

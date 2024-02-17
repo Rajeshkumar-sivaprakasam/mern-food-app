5525dbbc1de33cb7cae1e35502952ce5e898bc7b
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2024-02-17 12:23:38 +0530

    footer and responsive home page!

--- frontend/src/appRoutes.tsx
+++ frontend/src/appRoutes.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { Layout } from "./layout";
+import { Layout } from "./layout/Layout";
+import HomePage from "./pages/HomePage";
 
 export const AppRoutes = () => {
   return (
@@ -8,7 +9,7 @@ export const AppRoutes = () => {
         path="/"
         element={
           <Layout>
-            <span>Home page</span>
+            <HomePage />
           </Layout>
         }
       />

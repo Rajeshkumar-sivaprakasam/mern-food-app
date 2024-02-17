14b29fab87f6972e9ba1728de25e03104d6550ef
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2024-02-17 13:00:23 +0530

    desktop login logout added!

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,16 +1,23 @@
+import UserNameMenu from "./UserNameMenu";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
 const MainNav = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
-    <Button
-      variant="ghost"
-      className="font-bold hover:text-orange-500 hover:bg-white"
-      onClick={async () => await loginWithRedirect()}
-    >
-      Log In
-    </Button>
+    <span>
+      {isAuthenticated ? (
+        <UserNameMenu />
+      ) : (
+        <Button
+          variant="ghost"
+          className="font-bold hover:text-orange-500 hover:bg-white"
+          onClick={async () => await loginWithRedirect()}
+        >
+          Log In
+        </Button>
+      )}
+    </span>
   );
 };
 
